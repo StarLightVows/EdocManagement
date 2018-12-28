9b4dabbbc70ffad8fd47078348cf5714c0f08a8c
Xdy <731576822@qq.com>
2018-12-28 16:39:40 +0800

    打死了撒2

--- src/org/star/controller/IndexController.java
+++ src/org/star/controller/IndexController.java
@@ -1,22 +1,16 @@
 package org.star.controller;
 
-import java.util.List;
-
-import com.github.pagehelper.Page;
+import com.alibaba.fastjson.JSON;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.star.model.EdocCategory;
 import org.star.model.EdocEntry;
 import org.star.service.EdocCategoryService;
 import org.star.service.EdocEntryService;
 
-import com.alibaba.fastjson.JSON;
-import com.github.pagehelper.PageHelper;
-import com.github.pagehelper.PageInfo;
+import java.util.List;
 
 @RestController
 @RequestMapping("/index")
